2fa6b07285918f8f9e9abcf57f35d90034454ea2
mici-que <Maria.Kohler@ibm.com>
2022-06-17 12:34:50 +0200

    1 GREEN

--- SudokuSolutionValidator.py
+++ SudokuSolutionValidator.py
@@ -1,2 +1,16 @@
 def validSolution(sudoku=None):
-    pass
+    if sudoku == None:
+        return False
+    if not isinstance(sudoku, list):
+        return False
+    if len(sudoku) != 9:
+        return False
+    for row in sudoku:
+        if len(row) != 9:
+            return False
+        for item in row:
+            if not isinstance(item, int):
+                return False
+            if not 0 < item < 10:
+                return False
+    return True
